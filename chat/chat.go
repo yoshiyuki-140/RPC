aaab321f6b3606b4328b095f47ef27956e4f5fff
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-06-16 19:40:14 +0900

    update : チュートリアルは完成

--- chat/chat.go
+++ chat/chat.go
@@ -9,7 +9,7 @@ type Server struct {
 	UnimplementedChatServiceServer
 }
 
-func (s Server) sayHello(ctx context.Context, in *Message) (*Message, error) {
+func (s Server) SayHello(ctx context.Context, in *Message) (*Message, error) {
 	log.Printf("Receive message body rom client: %s", in.Body)
 	return &Message{Body: "Hello From the server"}, nil
 }
---
+++ client/client.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/yoshiyuki-140/RPC/chat"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":9000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := chat.NewChatServiceClient(conn)
+
+	resp, err := c.SayHello(context.Background(), &chat.Message{
+		Body: "Hello From Client!",
+	})
+	if err != nil {
+		log.Fatalf("Error when calling SayHello: %s", err)
+	}
+	log.Printf("Responce from server: %s", resp.Body)
+}
